cb6f08b63a71aeb1e6b679be85653507e90ebb95
jose valdez <josezepedavaldez30@gmail.com>
2023-01-27 19:04:07 -0800

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car(string makeInput, string modelInput, int yearInput)
+        { 
+        
+        }
+
+
+        public Car()
+    {
+
+
+        //Create a Make property of type string that is public
+        //Create a Model property of type string that is public
+        //Create a Year property of type int that is public
+       
+
+
+
+    }
+    public string Make { get; set; } // get = read set = write
+     public string Model { get; set; }
+     public int Year { get; set; }
+    
+   }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Classes
+﻿using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();// constructor special member method help us create new instance of the Cas class
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+
+            };
+            var chevy = new Car("Chevy", "Impala", 2000);
+            var carList = new List<Car>() { myCar, mazda, chevy };
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+             
         }
     }
 }
